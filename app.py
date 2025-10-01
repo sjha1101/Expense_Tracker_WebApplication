183593828cdd4c92b211da221e6e7f2bae47c074
Surbhi Jha <codingchannel8i@gmail.com>
2025-10-01 19:33:06 +0530

    correction app file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3, hashlib
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -148,4 +149,5 @@ def delete_expense(id):
 
 # ---------- Run App ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
